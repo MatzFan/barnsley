a86230a6495fcf0801943dd68c02d5a5479c8cf1
Bruce Steedman <bruce.steedman@gmail.com>
2015-09-24 13:44:20 +0100

    refactor, no record yet

--- barnsley.rb
+++ barnsley.rb
@@ -2,51 +2,62 @@
 
 require 'gosu'
 
+WIDTH = 600
+HEIGHT = 900
+
 class GameWindow < Gosu::Window
   def initialize
-    super 600, 600
-    self.caption = "Gosu Tutorial Game"
+    super WIDTH, HEIGHT
+    self.caption = 'Barnsley fern'
     @image = Gosu::Image.from_text('.', 8)
     @color = Gosu::Color.new(0xff_00ff00)
     @random = Random.new
+    @x, @y = 0, 0
     @points = []
+    @new_points_num = 400
+    @max_points = 360000
   end
 
-  def fern
-    if @points.size < 1000000
-      x, y = 0, 0
-      200.times do
-        rand = @random.rand
-        if rand < 0.01
-          x = 0
-          y = 0.16 * y
-        elsif rand < 0.84
-          newx = (0.85 * x) + (0.04 * y)
-          newy = (-0.04 * x) + (0.85 * y) + 1.6
-          x, y = newx, newy
-        elsif rand < 0.92
-          newx = (0.2 * x) - (0.26 * y)
-          newy = (0.23 * x) + (0.22 * y) + 1.6
-          x, y = newx, newy
-        else
-          newx = (-0.15 * x) + (0.28 * y)
-          newy = (0.26 * x) + (0.24 * y) + 0.44
-          x, y = newx, newy
-        end
-        @points << [x, y]
-      end
-    end
-    @points
+  def f1
+    @x, @y = 0, 0.16 * @y
+  end
+
+  def f2
+    @x, @y = 0.85 * @x + 0.04 * @y, -0.04 * @x + 0.85 * @y + 1.6
+  end
+
+  def f3
+    @x, @y = 0.2 * @x - 0.26 * @y, 0.23 * @x + 0.22 * @y + 1.6
+  end
+
+  def f4
+    @x, @y = -0.15 * @x + 0.28 * @y, 0.26 * @x + 0.24 * @y + 0.44
+  end
+
+  def function(probability)
+    return :f2 if probability < 0.85
+    return :f3 if probability < 0.92
+    return :f4 if probability < 0.99
+    :f1 # least probable last :)
+  end
+
+  def transpose(rand)
+    self.send function(rand)
+  end
+
+  def new_points(arr = [])
+    arr.size == @new_points_num ? arr : new_points(arr << transpose(rand))
   end
 
   def update
   end
 
   def draw
-    fern.each do |arr|
-      @x = (600/2) + (arr.first * 600/6)
-      @y = 600 - (arr.last * 600/11)
-      @image.draw(@x, @y, 1, 1, 1, @color) # x, y, z-order
+    @points.count < @max_points ? (@points += new_points) : puts('DONE')
+    @points.each do |arr|
+      @xs = WIDTH/2 + (arr.first * WIDTH/6)
+      @ys = HEIGHT - (arr.last * HEIGHT/10)
+      @image.draw(@xs, @ys, 1, 1, 1, @color) # x, y, z-order
     end
   end
 end
