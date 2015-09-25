1655a8a3028ab06177fdd04c503b7a2d154ef962
Bruce Steedman <bruce.steedman@gmail.com>
2015-09-25 16:44:45 +0100

    refactor with Procs

--- barnsley.rb
+++ barnsley.rb
@@ -13,36 +13,24 @@ class DrawWindow < Gosu::Window
     @color = Gosu::Color.new(0xff_00ff00)
     @random = Random.new
     @x, @y = 0, 0
+    @f1 = lambda {|x,y| x, y = 0, 0.16*y}
+    @f2 = lambda {|x,y| x, y = 0.85*x + 0.04*y, -0.04*x + 0.85*y + 1.6}
+    @f3 = lambda {|x,y| x, y = 0.2*x - 0.26*y, 0.23*x + 0.22*y + 1.6}
+    @f4 = lambda {|x,y| x, y = -0.15*x + 0.28*y, 0.26*x + 0.24*y + 0.44}
     @points = []
     @new_points_num = `ulimit -s`.chomp.to_i # stack level :)
     @max_points = 600000
   end
 
-  def f1
-    @x, @y = 0, 0.16 * @y
-  end
-
-  def f2
-    @x, @y = 0.85 * @x + 0.04 * @y, -0.04 * @x + 0.85 * @y + 1.6
-  end
-
-  def f3
-    @x, @y = 0.2 * @x - 0.26 * @y, 0.23 * @x + 0.22 * @y + 1.6
-  end
-
-  def f4
-    @x, @y = -0.15 * @x + 0.28 * @y, 0.26 * @x + 0.24 * @y + 0.44
-  end
-
-  def function(probability)
-    return :f2 if probability < 0.85
-    return :f3 if probability < 0.92
-    return :f4 if probability < 0.99
-    :f1 # least probable last :)
+  def function(probability) # Strategy pattern :)
+    return @f2 if probability < 0.85
+    return @f3 if probability < 0.92
+    return @f4 if probability < 0.99
+    @f1 # least probable last :)
   end
 
   def iterate(rand)
-    self.send function(rand)
+    @x, @y = function(rand).call(@x, @y)
   end
 
   def new_points(arr = [])
